bf82b6dc21bc20f479d5a42496c9f9e5a22f254c
Kristóf Levente Pápai <83229957+KristofPapai@users.noreply.github.com>
2021-11-15 16:53:43 +0100

    backup
    some features kinda work.

--- base.py
+++ base.py
@@ -1,5 +1,4 @@
 import math
-
 import numpy as np
 
 cities = ["Berlin","Moscow","Denver","Budapest","Tokyo","Helsinki"]
@@ -7,15 +6,21 @@ cities_cordinates = [[4,3],[2,4],[5,3],[7,2],[1,1],[3,11]]
 pathIndex = []
 
 def bruteForce(arr):
-    for rows in len(cities_cordinates):
-        for search in range(rows,len(cities_cordinates)):
-            
+    matrixLength = len(cities_cordinates)
+    for rows in range(0, matrixLength):
+        distances = cordinetDistance2nd(cities_cordinates, rows)
+        minValue = min(distances)
+        minIndex = distances.index(minValue)
+        print(minIndex)
+    return True
 
+def cordinetDistance2nd(arr,baseRow):
+    distances = []
+    arrLength = len(arr)
+    for i in range(1, arrLength):
+        distance = math.sqrt(((arr[baseRow][0]-arr[i][0])**2)+((arr[baseRow][1]-arr[i][1])**2))
+        print(distance)
+        distances.append(distance)
+    return distances
 
-
-
-def cordinateDistance(x1,y1,x2,y2):
-    firstX = math.pow((x1 - x2),2)
-    firstY = math.pow((y1 - y2),2)
-    distance = firstX + firstY
-    return math.sqrt(distance)
+print(bruteForce(cities_cordinates))
