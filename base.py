56784859cd526d3c2b75f7bf813d5f9701b5149a
Kristóf Levente Pápai <83229957+KristofPapai@users.noreply.github.com>
2021-11-15 20:11:05 +0100

    still not working but its fine
    Gona debug it an other day

--- base.py
+++ base.py
@@ -1,19 +1,14 @@
 import math
-
 import numpy as np
 
 cities = ["Berlin","Moscow","Denver","Budapest","Tokyo","Helsinki"]
 cities_cordinates = [[4,3],[2,4],[5,3],[7,2],[1,1],[3,11]]
 pathIndex = []
-#print(cities_cordinates[3][0])
-#retardált vagyok. Szarul használtam az indexet! thx c#!!!
-#ha átléptünk a következő node-ra akkor töröljük azt  előző node-ot a mátrixbol
+
 
 def bruteForce(arr):
     matrixLength = len(arr)
     pathTaken = []
-    #lehet while ciklus kéne inkább
-
     rows = 0
     while(len(pathTaken) != len(arr)):
         distances = cordinetDistance2nd(arr, rows)
@@ -28,10 +23,6 @@ def bruteForce(arr):
     print(pathTaken)
     return True
 
-
-#ez itt végül lehet nem kell majd
-
-
 def cordinetDistance2nd(arr,baseRow):
     distances = []
     arrLength = len(arr)
@@ -42,6 +33,3 @@ def cordinetDistance2nd(arr,baseRow):
     return distances
 
 print(bruteForce(cities_cordinates))
-
-
-
