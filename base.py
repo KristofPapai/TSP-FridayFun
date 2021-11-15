a263087fc8ab7dca5cc5e2b4997bfea2294fb74f
Kristóf Levente Pápai <83229957+KristofPapai@users.noreply.github.com>
2021-11-15 17:03:29 +0100

    backup2
    getting close

--- base.py
+++ base.py
@@ -5,11 +5,12 @@ cities = ["Berlin","Moscow","Denver","Budapest","Tokyo","Helsinki"]
 cities_cordinates = [[4,3],[2,4],[5,3],[7,2],[1,1],[3,11]]
 pathIndex = []
 
+
 def bruteForce(arr):
     matrixLength = len(cities_cordinates)
     for rows in range(0, matrixLength):
         distances = cordinetDistance2nd(cities_cordinates, rows)
-        minValue = min(distances)
+        minValue = min(i for i in distances if i > 0)
         minIndex = distances.index(minValue)
         print(minIndex)
     return True
@@ -19,8 +20,8 @@ def cordinetDistance2nd(arr,baseRow):
     arrLength = len(arr)
     for i in range(1, arrLength):
         distance = math.sqrt(((arr[baseRow][0]-arr[i][0])**2)+((arr[baseRow][1]-arr[i][1])**2))
-        print(distance)
         distances.append(distance)
+    print(distances)
     return distances
 
 print(bruteForce(cities_cordinates))
