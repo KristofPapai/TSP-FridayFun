e5e89895a39115a4ae906057c83ae7f46ca14bda
Kristóf Levente Pápai <83229957+KristofPapai@users.noreply.github.com>
2021-11-15 20:08:11 +0100

    not working properly

--- base.py
+++ base.py
@@ -1,27 +1,47 @@
 import math
+
 import numpy as np
 
 cities = ["Berlin","Moscow","Denver","Budapest","Tokyo","Helsinki"]
 cities_cordinates = [[4,3],[2,4],[5,3],[7,2],[1,1],[3,11]]
 pathIndex = []
-
+#print(cities_cordinates[3][0])
+#retardált vagyok. Szarul használtam az indexet! thx c#!!!
+#ha átléptünk a következő node-ra akkor töröljük azt  előző node-ot a mátrixbol
 
 def bruteForce(arr):
-    matrixLength = len(cities_cordinates)
-    for rows in range(0, matrixLength):
-        distances = cordinetDistance2nd(cities_cordinates, rows)
+    matrixLength = len(arr)
+    pathTaken = []
+    #lehet while ciklus kéne inkább
+
+    rows = 0
+    while(len(pathTaken) != len(arr)):
+        distances = cordinetDistance2nd(arr, rows)
         minValue = min(i for i in distances if i > 0)
         minIndex = distances.index(minValue)
-        print(minIndex)
+        if(minIndex in pathTaken):
+            minValue = min(i for i in distances if i != minValue and i > 0)
+            minIndex = distances.index(minValue)
+        rows = minIndex
+        #print(minIndex)
+        pathTaken.append(minIndex)
+    print(pathTaken)
     return True
+
+
+#ez itt végül lehet nem kell majd
+
 
 def cordinetDistance2nd(arr,baseRow):
     distances = []
     arrLength = len(arr)
-    for i in range(1, arrLength):
+    for i in range(0, arrLength):
         distance = math.sqrt(((arr[baseRow][0]-arr[i][0])**2)+((arr[baseRow][1]-arr[i][1])**2))
         distances.append(distance)
     print(distances)
     return distances
 
 print(bruteForce(cities_cordinates))
+
+
+
