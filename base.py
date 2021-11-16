06c4409e9203c7c585275cedd9b9a97e170a1dfc
Kristóf Levente Pápai <83229957+KristofPapai@users.noreply.github.com>
2021-11-16 21:07:10 +0100

    kinda working.
    its not perfect have some bugs i still have to solve the indexing issue

--- base.py
+++ base.py
@@ -1,4 +1,5 @@
 import math
+
 import numpy as np
 
 cities = ["Berlin","Moscow","Denver","Budapest","Tokyo","Helsinki"]
@@ -9,19 +10,38 @@ pathIndex = []
 def bruteForce(arr):
     matrixLength = len(arr)
     pathTaken = []
+    #lehet while ciklus kéne inkább
+    usedMinValues = []
+
+    #TALÁN ITT VAN PROBLÉMA?
     rows = 0
     while(len(pathTaken) != len(arr)):
         distances = cordinetDistance2nd(arr, rows)
-        minValue = min(i for i in distances if i > 0)
+        forbiddenFruit = distances[0]
+        #itt lehet rosszul kezelem az utolso viszgaltok
+        #mindindex nem lehet 0-a mert az a kiindulo pont
+        #EMLÉKEZTETŐ I EGY ÉRTÉK NEM PEDIG EGY INDEX.....
+        #VALAHOL AZ A GOND HOGY A KEZDŐPONT BEKAVAR
+        minValue = min(i for i in distances if i > 0 and i != forbiddenFruit)
         minIndex = distances.index(minValue)
-        if(minIndex in pathTaken):
-            minValue = min(i for i in distances if i != minValue and i > 0)
+        if(minIndex in pathTaken or minValue in usedMinValues):
+            minValue = min(i for i in distances if i not in usedMinValues and i > 0 and i != forbiddenFruit)
             minIndex = distances.index(minValue)
+            usedMinValues.append(minValue)
+        else:
+            print("belép ide valaha ez a szar?")
+            usedMinValues.append(minValue)
         rows = minIndex
-        #print(minIndex)
         pathTaken.append(minIndex)
-    print(pathTaken)
+        print(minIndex)
+    print("--------------------")
+    print("Útvonal: ",pathTaken)
+    print("Értékek: ",usedMinValues)
     return True
+
+
+#ez itt végül lehet nem kell majd
+
 
 def cordinetDistance2nd(arr,baseRow):
     distances = []
@@ -32,4 +52,13 @@ def cordinetDistance2nd(arr,baseRow):
     print(distances)
     return distances
 
+
+#lehet irni kéne egy metodust ami ujragenerálja a distance mátrixot
+#és removolgatjuk az eredeti mátrixbol az értékeket
+
+
+
 print(bruteForce(cities_cordinates))
+
+
+
